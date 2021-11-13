fdb86b69b3096a8dc44309519ef42e6c6d179b42
AloeSwap <info@aloeswap.com>
2021-11-13 01:05:16 +0100

    Fix wallet address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ module.exports = async function(deployer) {
   await deployer.deploy(RichPigToken);
   const richPigToken = await RichPigToken.deployed();
   await deployer.link(RichPigToken, MasterChef);
-  const feeAddress = "0x650E5c6071f31065d7d5Bf6CaD5173819cA72c41";
+  const feeAddress = "0xF3E0B0b7A57C70CB2876Aa6C763eB2668fC8BF20";
   const tokenPerBlock = "1000000000000000000";
   const startBlock = 1;
   await deployer.deploy(MasterChef, richPigToken.address, feeAddress, tokenPerBlock, startBlock);
